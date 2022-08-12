1267b78335d4649c7dd2860a998929e85cf75ea0
Afshin Paydar <afshin.paydar@binary.com>
2022-08-12 15:23:45 +0800

    fix CSRF

--- source_code/server.py
+++ source_code/server.py
@@ -1,11 +1,14 @@
 import re
 from flask import Flask, flash, render_template, redirect, url_for, request, session, Response
+from flask_wtf.csrf import CSRFProtect
 from flask_cors import CORS, cross_origin
 from module.database import Database
 import os
 
 app = Flask(__name__)
-app.config['WTF_CSRF_ENABLED'] = True 
+app.config['WTF_CSRF_ENABLED'] = True
+csrf = CSRFProtect()
+csrf.init_app(app)
 app._static_folder = "static"
 app.secret_key = os.getenv('API_SECRET_KEY')
 db = Database()
