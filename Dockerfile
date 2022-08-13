87b89d01060b781789175dd234d5fd7ce6b988d9
Afshin Paydar <afshin.paydar@binary.com>
2022-08-13 18:31:22 +0800

    disable sidecar nginx

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM python:3.11-rc-slim
 
+EXPOSE 80
 WORKDIR /app
 COPY source_code/ /app
 RUN pip install --root-user-action=ignore -r requirements.txt
--- source_code/server.py
+++ source_code/server.py
@@ -99,4 +99,4 @@ def page_not_found():
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    serve(app, host="0.0.0.0", port=80)
